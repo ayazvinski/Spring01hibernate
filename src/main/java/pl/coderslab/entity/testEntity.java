703b40d22a425b6b17f955bc9e6dd5f8b0577c04
ayazvinski <a.yazvinski@limitless.pl>
2024-01-13 14:05:34 +0100

    Zadanie 1,2

---
+++ src/main/java/pl/coderslab/entity/testEntity.java
@@ -0,0 +1,14 @@
+package pl.coderslab.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class testEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
