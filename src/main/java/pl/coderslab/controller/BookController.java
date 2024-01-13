0b95a24b29b128fb7bd76e14f67466fad72050bd
ayazvinski <a.yazvinski@limitless.pl>
2024-01-13 16:55:49 +0100

    Create + getById + Edit

---
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -0,0 +1,48 @@
+package pl.coderslab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.dao.BookDao;
+import pl.coderslab.entity.Book;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+    private BookDao bookDao;
+
+    public BookController(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    @PostMapping("/create")
+    @ResponseBody
+    public String createBook(@RequestParam String title,
+                             @RequestParam int rating,
+                             @RequestParam String description
+    ) {
+        Book b = new Book(title, rating, description);
+        b = bookDao.create(b);
+        return b.toString();
+    }
+
+    @PostMapping("/edit/{id}")
+    @ResponseBody
+    public String editBook(@PathVariable long id,
+                           @RequestParam String title,
+                           @RequestParam int rating,
+                           @RequestParam String description){
+        Book book = bookDao.findById(id);
+        book.setTitle(title);
+        book.setRating(rating);
+        book.setDescription(description);
+        bookDao.edit(book);
+        return book.toString();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public String getById(@PathVariable long id) {
+        Book book = bookDao.findById(id);
+        return book.toString(); // todo handle book NOT found exception
+    }
+}
---
+++ src/main/java/pl/coderslab/entity/Book.java
@@ -0,0 +1,62 @@
+package pl.coderslab.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private int rating;
+    private String description;
+
+    public Book() {
+
+    }
+
+    public Book(String title, int rating, String description) {
+        this.title = title;
+        this.rating = rating;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", rating=" + rating +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
